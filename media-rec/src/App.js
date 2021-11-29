8bc5ac622f730611d6a05a9d37f19821b2227d50
Bader Alrifai <bedrrifai1@gmail.com>
2021-11-29 03:39:38 -0700

    Created the side nav and added material theme component library and icons.

--- media-rec/src/App.js
+++ media-rec/src/App.js
@@ -9,41 +9,43 @@ import Nav from './components/Nav';
 import {BrowserRouter as Router, Routes, Route} from 'react-router-dom';
 
 function App() {
-	let name = "Hero Academy"
-	let summary = "What would the world be like if 80 percent of the population " +
-				  "manifested extraordinary superpowers called “Quirks” at age four? Heroes and villains would " +
-				  "be battling it out everywhere! Becoming a hero would mean learning to use your power, but " +
-				  "where would you go to study? U.A. High's Hero Program of course! But what would you do if " +
-				  "you were one of the 20 percent who were born Quirkless?"
-	let tags = ["anime ", "tv ", "show "]
-	let genres = ["action", "drama"]
-	let recReview = "Middle school student Izuku Midoriya wants to be a hero more than anything, but he " +
-					"hasn't got an ounce of power in him. With no chance of ever getting into the prestigious " +
-					"U.A. High School for budding heroes, his life is looking more and more like a dead end. " +
-					"Then an encounter with All Might, the greatest hero of them all gives him a chance to " +
-					"change his destiny…"
-	let recRating = 6.9
-	let comRating = 4.2
-
 	return (
-		<ThemeProvider theme={theme}>
-			<Typography variant="h1" color="primary" light="true">Hello past Ninad, how are you?</Typography>
-			{/* <MediaCard
-				name={name}
-				summary={summary}
-				tags={tags}
-				genres={genres}
-				recReview={recReview}
-				recRating={recRating}
-				comRating={comRating}
-			/> */}
+		<ThemeProvider theme={theme} >
 			<Router>
-				<Nav />
-				<Routes>
-					<Route path="/" element={<Home />}/>
-					<Route path="/friends" element={<Friends />}/>
-					<Route path="/notifications" element={<Notifications />}/>
-				</Routes>
+				<Nav>
+					<Routes>
+						<Route path="/" element={<Home />}/>
+						<Route path="/friends" element={<Friends />}/>
+						<Route path="/notifications" element={<Notifications />}/>
+					</Routes>
+					<Typography paragraph>
+						Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
+						tempor incididunt ut labore et dolore magna aliqua. Rhoncus dolor purus non
+						enim praesent elementum facilisis leo vel. Risus at ultrices mi tempus
+						imperdiet. Semper risus in hendrerit gravida rutrum quisque non tellus.
+						Convallis convallis tellus id interdum velit laoreet id donec ultrices.
+						Odio morbi quis commodo odio aenean sed adipiscing. Amet nisl suscipit
+						adipiscing bibendum est ultricies integer quis. Cursus euismod quis viverra
+						nibh cras. Metus vulputate eu scelerisque felis imperdiet proin fermentum
+						leo. Mauris commodo quis imperdiet massa tincidunt. Cras tincidunt lobortis
+						feugiat vivamus at augue. At augue eget arcu dictum varius duis at
+						consectetur lorem. Velit sed ullamcorper morbi tincidunt. Lorem donec massa
+						sapien faucibus et molestie ac.
+					</Typography>
+					<Typography paragraph>
+						Consequat mauris nunc congue nisi vitae suscipit. Fringilla est ullamcorper
+						eget nulla facilisi etiam dignissim diam. Pulvinar elementum integer enim
+						neque volutpat ac tincidunt. Ornare suspendisse sed nisi lacus sed viverra
+						tellus. Purus sit amet volutpat consequat mauris. Elementum eu facilisis
+						sed odio morbi. Euismod lacinia at quis risus sed vulputate odio. Morbi
+						tincidunt ornare massa eget egestas purus viverra accumsan in. In hendrerit
+						gravida rutrum quisque non tellus orci ac. Pellentesque nec nam aliquam sem
+						et tortor. Habitant morbi tristique senectus et. Adipiscing elit duis
+						tristique sollicitudin nibh sit. Ornare aenean euismod elementum nisi quis
+						eleifend. Commodo viverra maecenas accumsan lacus vel facilisis. Nulla
+						posuere sollicitudin aliquam ultrices sagittis orci a.
+					</Typography>
+				</Nav>
 			</Router>
 		</ThemeProvider>
 	);
--- media-rec/src/components/Nav.js
+++ media-rec/src/components/Nav.js
@@ -1,22 +1,92 @@
-import React from 'react'
+import React from "react";
 import { Link } from "react-router-dom"
+import { styled } from "@mui/material/styles";
+import Box from "@mui/material/Box";
+import MuiDrawer from "@mui/material/Drawer";
+import List from "@mui/material/List";
+import CssBaseline from "@mui/material/CssBaseline";
+import Divider from "@mui/material/Divider";
+import ListItem from "@mui/material/ListItem";
+import ListItemIcon from "@mui/material/ListItemIcon";
+import ListItemText from "@mui/material/ListItemText";
+import GroupIcon from "@mui/icons-material/Group";
+import NotificationsIcon from "@mui/icons-material/Notifications";
+import FormatListBulletedIcon from "@mui/icons-material/FormatListBulleted";
 
-function Nav() {
+const drawerWidth = 240;
+
+const openedMixin = () => ({
+    width: drawerWidth,
+    backgroundColor: "#1B1E28",
+    color: "#CBE0F2",
+    overflowX: "hidden",
+});
+
+const DrawerHeader = styled("div")(({ theme }) => ({
+    display: "flex",
+    alignItems: "center",
+    justifyContent: "center",
+    fontSize: 24,
+    padding: theme.spacing(0, 1),
+    ...theme.mixins.toolbar,
+}));        
+
+const Drawer = styled(MuiDrawer, { shouldForwardProp: (prop) => prop !== "open" })(
+    ({ theme, open }) => ({
+        width: drawerWidth,
+        flexShrink: 0,
+        whiteSpace: "nowrap",
+        boxSizing: "border-box",
+        ...(open && {
+            ...openedMixin(theme),
+            "& .MuiDrawer-paper": openedMixin(),
+        })
+    }),
+);
+
+const Nav = ({ children }) => {
     return (
-        <div>
-            <ul>
-                <Link to="/">
-                    <li>Home Page</li>
-                </Link>
-                <Link to="friends">
-                    <li>Friends Page</li>
-                </Link>
-                <Link to="notifications">
-                    <li>Notifications Page</li>
-                </Link>
-            </ul>
-        </div>
-    )
+        <Box sx={{ display: "flex" }}>
+            <CssBaseline />
+            <Drawer variant="permanent" open={true}>
+                <DrawerHeader>
+                    MediaRec
+                </DrawerHeader>
+                
+                <Divider />
+
+                <List>
+                    <Link style={{ textDecoration: 'none', color:"#CBE0F2" }} to="/">
+                        <ListItem button key="MediaList">
+                            <ListItemIcon>
+                                <FormatListBulletedIcon color="primary"/>
+                            </ListItemIcon>
+                            <ListItemText primary="Media List"/>
+                        </ListItem>
+                    </Link>
+                    <Link style={{ textDecoration: 'none', color:"#CBE0F2" }} to="friends">
+                        <ListItem button key="Friends">
+                            <ListItemIcon>
+                                <GroupIcon color="primary"/>
+                            </ListItemIcon>
+                            <ListItemText primary="Friends"/>
+                        </ListItem>
+                    </Link>
+                    <Link style={{ textDecoration: 'none', color:"#CBE0F2" }} to="notifications">
+                        <ListItem button key="Notifications">
+                            <ListItemIcon>
+                                <NotificationsIcon color="primary"/>
+                            </ListItemIcon>
+                            <ListItemText primary="Notifications"/>
+                        </ListItem>
+                    </Link>
+                </List>
+            </Drawer>
+            <Box component="main" sx={{ flexGrow: 1, p: 3}} backgroundColor="#161923" color="#CBE0F2">
+                {children}
+            </Box>
+        </Box>
+    );
 }
 
 export default Nav
