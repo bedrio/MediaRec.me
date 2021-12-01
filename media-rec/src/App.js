cc32d3c5137800fd4366f792b8a2bed118fbc154
ninkuk <ninadk03@gmail.com>
2021-12-01 15:36:56 -0700

    Started work on authentication page

--- media-rec/src/App.js
+++ media-rec/src/App.js
@@ -1,28 +1,33 @@
-import './themes/App.css';
-import {theme} from "./themes/AppTheme";
-import {ThemeProvider, Typography} from "@material-ui/core";
-import MediaCard from "./components/MediaCard"
-import RecMediaCard from "./components/RecMediaCard"
+import "./themes/App.css";
+import { theme } from "./themes/AppTheme";
+import { ThemeProvider, Typography } from "@material-ui/core";
 import Friends from "./pages/Friends";
 import WatchList from "./pages/WatchList";
 import Notifications from "./pages/Notifications";
-import Nav from './components/Nav';
-import {BrowserRouter as Router, Routes, Route} from 'react-router-dom';
+import Nav from "./components/Nav";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Authentication from "./pages/Authentication";
 
 function App() {
-	return (
-		<ThemeProvider theme={theme} >
-			<Router>
-				<Nav>
-					<Routes>
-						<Route path="/" element={<WatchList />}/>
-						<Route path="/friends" element={<Friends />}/>
-						<Route path="/notifications" element={<Notifications />}/>
-					</Routes>
-				</Nav>
-			</Router>
-		</ThemeProvider>
-	);
+  return (
+    <ThemeProvider theme={theme}>
+      <Authentication />
+    </ThemeProvider>
+  );
+}
+
+function AppNavigation() {
+  return (
+    <Router>
+      <Nav>
+        <Routes>
+          <Route path="/" element={<WatchList />} />
+          <Route path="/friends" element={<Friends />} />
+          <Route path="/notifications" element={<Notifications />} />
+        </Routes>
+      </Nav>
+    </Router>
+  );
 }
 
 export default App;
---
+++ media-rec/src/pages/Authentication.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function Authentication() {
+    return (
+      <></>  
+    );
+}
+
+export default Authentication
