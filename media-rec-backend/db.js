68eb9e1cacafa2bafbf35a862d60039155e19f4c
ninkuk <ninadk03@gmail.com>
2021-12-02 11:53:56 -0700

    changed the db host

--- media-rec-backend/db.js
+++ media-rec-backend/db.js
@@ -3,7 +3,7 @@ const Pool = require("pg").Pool;
 const pool = new Pool({
     user: "postgres",
     password: "28uVYGf2JfM6LrXX",
-    host: "localhost",
+    host: "database-2.cg8r25usay7f.us-east-2.rds.amazonaws.com",
     port: 5432,
     database: "userdata"
 });
