3e9c4c23f589411cc956538801280f86902a7e22
Ninad Kulkarni <ninadk03@gmail.com>
2021-12-02 19:27:52 -0700

    created basic friends layout

--- media-rec/src/pages/Friends.jsx
+++ media-rec/src/pages/Friends.jsx
@@ -1,9 +1,41 @@
+// import { } from '@material-ui/core'
+import { Avatar, Button, Grid, IconButton, List, ListItemAvatar, ListItemText, Typography, ListItem } from '@mui/material';
 import React from 'react'
+import FolderIcon from '@mui/icons-material/Folder';
+import DeleteIcon from '@mui/icons-material/Delete';
+import { useState } from 'react';
 
 function Friends() {
+    const [friends, setFriends] = useState([{ "email": "bruh", "name": "yo" }, { "email": "bruh", "name": "yo" }, { "email": "bruh", "name": "yo" }]);
+
     return (
         <>
-            <h1>Friends</h1>
+            <h1>My Friends</h1>
+            <div style={{ width: "25%" }}>
+                <List>
+                    {
+                        friends.map(friend => (
+                            <ListItem
+                                secondaryAction={
+                                    <IconButton edge="end" aria-label="delete">
+                                        <DeleteIcon />
+                                    </IconButton>
+                                }
+                            >
+                                <ListItemAvatar>
+                                    <Avatar>
+                                        <FolderIcon />
+                                    </Avatar>
+                                </ListItemAvatar>
+                                <ListItemText
+                                    primary={friend.name}
+                                    secondary={friend.email}
+                                />
+                            </ListItem>
+                        ))
+                    }
+                </List>
+            </div>
         </>
     )
 }
